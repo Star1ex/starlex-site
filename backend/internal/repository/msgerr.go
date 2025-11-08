fe06d9c41b0e5b093f000a3909e432b0f850bab6
Zakhar <zandreev217@gmail.com>
2025-11-08 19:54:14 +0700

    add GetUserByEmail(Login) method

--- backend/internal/repository/msgerr.go
+++ backend/internal/repository/msgerr.go
@@ -3,3 +3,4 @@ package repository
 import "errors"
 
 var ErrAlreadyExists error = errors.New("repository: user with this email already exists")
+var ErrUserNotFound error = errors.New("repository: user with this email not found")
--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"context"
+	"errors"
+
 	"github.com/Team-Tracks/team-track-site/internal/domain/user"
 	"gorm.io/gorm"
 )
@@ -41,4 +44,25 @@ func NewRepository(db *gorm.DB)*UserRepository{
 	return &UserRepository{db: db}
 }
 
+func (r *UserRepository) CreateUser(ctx context.Context,u *user.User)error{
+	err:=r.db.WithContext(ctx).Create(fromDomain(u)).Error
+	if err!=nil{
+		if errors.Is(err, gorm.ErrDuplicatedKey){
+			return ErrAlreadyExists
+		}
+		return err
+	}
+	return nil
+}
 
+func (r *UserRepository) GetUserByEmail(ctx context.Context,email string)(*user.User,error){
+	var model User
+	result:=r.db.WithContext(ctx).Where("email = ?", email).First(&model)
+	if result.Error != nil{
+		if errors.Is(result.Error,gorm.ErrRecordNotFound){
+			return nil,ErrUserNotFound
+		}
+		return nil,result.Error
+	}
+	return toDomain(&model),nil
+}
