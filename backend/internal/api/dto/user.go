d407a34a7bd9f45d1bde5b3361e885263a0ab603
Zakhar <zandreev217@gmail.com>
2025-11-10 22:02:47 +0700

    handlers improvements

--- backend/internal/api/dto/user.go
+++ backend/internal/api/dto/user.go
@@ -3,16 +3,22 @@ package dto
 import "github.com/Team-Tracks/team-track-site/internal/domain/user"
 
 type UserApi struct {
-	ID        string `json:"id"`
 	Email     string `json:"email" binding:"required"`
 	Password  string `json:"password" binding:"required"`
-	FirstName string `jsob:"first_name"`
+	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
+}
+
+
+type UserResponse struct {
+    ID        string `json:"id"` 
+    Email     string `json:"email"`
+    FirstName string `json:"first_name"`
+    LastName  string `json:"last_name"`
 }
 
 func ToUserApi(u *user.User) *UserApi {
 	return &UserApi{
-		ID:        u.ID,
 		Email:     u.Email,
 		Password:  u.Password,
 		FirstName: u.FirstName,
@@ -22,10 +28,18 @@ func ToUserApi(u *user.User) *UserApi {
 
 func FromUserApi(u *UserApi) *user.User {
 	return &user.User{
-		ID:        u.ID,
 		Email:     u.Email,
 		Password:  u.Password,
 		FirstName: u.FirstName,
 		LastName:  u.LastName,
+	}
+}
+
+func ToUserResponse(u *user.User) *UserResponse{
+	return  &UserResponse{
+		ID: u.ID,
+		Email: u.Email,
+		FirstName: u.FirstName,
+		LastName: u.LastName,
 	}
 }
