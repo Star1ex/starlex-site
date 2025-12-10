372be02686022f14a29211550e4c4e18ad60234a
critiq17 <critiq17@gmail.com>
2025-12-10 22:10:32 +0100

    change home page

--- react-frontend/src/pages/home/HomePage.tsx
+++ react-frontend/src/pages/home/HomePage.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/shared/ui/Button.js";
 
-// HomePage first page which user can see
 export const HomePage = () => {
   const navigate = useNavigate();
 
@@ -11,15 +10,20 @@ export const HomePage = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-[#FFD1C1]">
-      <h1 className="text-7xl text-[#60392f] font-serif mb-6">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white transition-colors duration-300">
+      <h1 className="text-7xl text-black font-serif mb-6 transition-colors duration-300">
         Welcome to TeamTrack
       </h1>
-      <p className="text-[#60392f] font-serif ">
+      <p className="text-black font-serif mb-8 transition-colors duration-300">
         Open Source minimalistic task-manager.
       </p>
-      <div className="gap-4">
-        <Button variant={"secondary"} size={"md"} onClick={handleGetStarted}>
+      <div className="gap-4 flex">
+        <Button
+          variant={"secondary"}
+          size={"md"}
+          onClick={handleGetStarted}
+          className="bg-black text-white hover:bg-gray-800 transition-colors duration-200"
+        >
           Get Started
         </Button>
       </div>
