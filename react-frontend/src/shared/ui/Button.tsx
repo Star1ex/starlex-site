c25458019665425d0736fae3d5ca835f26ced090
critiq17 <critiq17@gmail.com>
2025-12-10 22:36:00 +0100

    also change button color

--- react-frontend/src/shared/ui/Button.tsx
+++ react-frontend/src/shared/ui/Button.tsx
@@ -2,15 +2,14 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/shared/lib/cn.js";
 import React, { type ButtonHTMLAttributes } from "react";
 
-
 const buttonStyles = cva(
   "inline-flex items-center justify-center rounded-xl font-medium transition active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed",
   {
     variants: {
       variant: {
-        primary: "bg-black text-white hover:bg-gray-800",
-        secondary: "bg-black text-white shadow-md hover:bg-gray-800 w-full py-3 font-semibold rounded-md transition-colors duration-200",
-        outline: "border border-black text-black hover:bg-gray-200",
+        primary: "bg-black text-white hover:bg-black/80",
+        secondary: "bg-black text-white w-full py-3 font-semibold rounded-md transition-colors duration-200 hover:bg-black/80 shadow-none",
+        outline: "border border-black text-black hover:bg-black/10",
       },
       size: {
         sm: "px-3 py-1 text-sm",
